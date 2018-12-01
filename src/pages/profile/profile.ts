4fac14bab9f638a2523f54107c25c8ad5227a861
Adam <acornachione@alaska.edu>
2018-11-30 20:49:53 -0800

    user profile can create a destructive new document in humanProfile collection with userId. Need to tie the data from the html input to the values created in the document

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, NavController, NavParams, Item, Platform } from 'ionic-angular';
 import { Events } from 'ionic-angular';
 
 import { AngularFireDatabase } from '@angular/fire/database';
+import { Observable } from 'rxjs/Observable';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+
 /**
  * Generated class for the ProfilePage page.
  *
@@ -16,17 +19,105 @@ import { AngularFireDatabase } from '@angular/fire/database';
   templateUrl: 'profile.html',
 })
 export class ProfilePage {
-	uid: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public events: Events) {
+	//Variables on Page:
+	@ViewChild('fName') fName;
+	@ViewChild('lName') lName;
+
+	//variables for ProfilePage
+	private itemDoc: AngularFirestoreDocument<Item>;
+	item: Observable<Item>;			//Firebase Document
+	uid: string;								//User id
+	items: Observable<any[]>; 	//Collections
 	
+	fields: Item;
+	private userName: string;	//Should be passed similar to uid
+	private firstName: string;	//Fetched from Firebase document
+	private lastName: string;	//Fetched from Firebase document
+	private dogPhotoPath: string;	//Fetched from Firebase image
+	private humanPhotoPath: string;	//Fetched from Firebase image
+
+	//Guiding fields:
+	private lookingFor: string; //dog park, hiking buddy, etc.
+	private characterDescription: string;
+	private ownerLikesAndDislikes: string;
+
+	//Dog info
+	dogs: Observable<any[]>; 	// Collection from firebase
+
+	//Doc exists?
+	private docExists: boolean;
+
+
+	//Constructor, includes ctrls, etc..
+  constructor(public navCtrl: NavController, public navParams: NavParams, public events: Events, public   afDB: AngularFireDatabase, private afs: AngularFirestore, private platform: Platform) {
+
 	this.events.subscribe('data:created', (data) => {	//Gets uid passed into from login page
-		console.log( data);
+		//console.log( data);
 		this.uid = data;
+
+		//once the user id is found, we can see if the user's document exists
+		//CHeck if document exists:
+		//NEED TO SWITCH TO THIS STATEMENT ONLY
+		afs.doc<Item>('humanProfile/'+this.uid).valueChanges().subscribe( res => {
+			if(res){
+				this.docExists = true;
+				console.log("doc: humanProfile/" + this.uid + " found!");
+				console.log("doc: humanProfile/" + this.uid + " found!");
+				//This line binds a Document tothe user
+				this.itemDoc = afs.doc<Item>('humanProfile/yoh59JUTatWayKRim3Th');
+				this.itemDoc.valueChanges().forEach( data => { 
+					this.firstName = data["firstName"],
+					this.fName = this.firstName,
+					this.lastName = data["lastName"],
+					this.lName = this.lastName
+				}); //Access by value
+			}
+			else{
+				this.docExists = false;
+				console.log("doc: humanProfile/" + this.uid + " not found");
+			}
+
+		});
+
+		//CHeck if document exists:
+		afs.doc<Item>('humanProfile/'+'yoh59JUTatWayKRim3Th').valueChanges().subscribe( res => {
+			if(res){
+				console.log("doc: humanProfile/" + 'yoh59JUTatWayKRim3Th' + " found!");
+				//This line binds a Document tothe user
+				this.itemDoc = afs.doc<Item>('humanProfile/yoh59JUTatWayKRim3Th');
+				this.itemDoc.valueChanges().forEach( data => { 
+					this.firstName = data["firstName"],
+					this.fName = this.firstName,
+					this.lastName = data["lastName"],
+					this.lName = this.lastName
+				}); //Access by value
+			}
+			else{
+				console.log("doc: humanProfile/" + 'yoh59JUTatWayKRim3Th' + " not found");
+			}
+
+		});
+
 	});
 	
-	
 
+	//This line binds items to the collection humanProfile
+	//this.items = afs.collection('humanProfile').valueChanges();
+	//console.log(this.items);
+
+
+
+
+		
+  }
+
+	//this.item = this.itemDoc.valueChanges().pipe();
+	//console.log(this.itemDoc.get);
+	//console.log(this.item)
+	
+	
+	
 	//this.userDoc = fireStore.doc<any>('humanProfile/' + this.uid);	//Creates a document in database with their user ID
 	/*this.userDoc.set({
     firstName: 'Allison',
@@ -35,12 +126,30 @@ export class ProfilePage {
 	dogID: '',
     // Other info you want to add here
   })*/
-  }
+  //}
   
   
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ProfilePage');
-  }
+		console.log('ionViewDidLoad ProfilePage');
+}
 
+//Updating methods
+	updateFirstName(){
+		console.log('New First name: ' + this.fName)
+	}
+
+	updateLastName(){
+		console.log('New Last name: ' + this.lName)
+	}
+
+	updateInfo(){
+		//This line will create a document in humanProfile collection with the ID
+		this.afs.collection('humanProfile').doc(this.uid).set({
+			firstName: this.fName,
+			lastNamae: this.lName
+		}
+		);
+
+	}
 }
